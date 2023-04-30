1cedc5be146a04d1afb10858d0b82c9da01710e2
qalokoz <qaisskhan10@hotmail.com>
2023-04-30 11:31:04 -0400

    method comments

--- jsc_caching.java
+++ jsc_caching.java
@@ -2,15 +2,19 @@ import org.apache.jcs.JCS;
 import org.apache.jcs.access.exception.CacheException;
 import java.util.List;
 
-/**
- * Dependency
- * <dependency>
-    <groupId>org.apache.jcs</groupId>
-    <artifactId>jcs</artifactId>
-    <version>2.3</version>
-</dependency>
- */
+ /**
+  * The cache method takes a List of Object arrays and stores it in a cache region called "myCacheRegion" 
+  using the JCS library's put method. The getCachedList method retrieves the cached list from the same 
+  cache region using the get method.
 
+  Dependency
+            <dependency>
+                <groupId>org.apache.jcs</groupId>
+                <artifactId>jcs</artifactId>
+                <version>2.3</version>
+            </dependency>
+
+  */
 public class CacheList {
     private static final String CACHE_REGION = "myCacheRegion";
 
